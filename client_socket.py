1962faaac0ba356aadc3747ef9dba4fae29b8de3
skihyeon <hero981001@gmail.com>
2024-01-02 19:36:32 +0900

    Refactoring Code

--- client_socket.py
+++ client_socket.py
@@ -1,28 +1,49 @@
-from socket import *
+import socket
 
-client_sock = socket(AF_INET, SOCK_STREAM)
+class ChatClient:
+    def __init__(self, host='127.0.0.1', port=8080):
+        self.server_addr = (host, port)
+        self.client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_addr = ('127.0.0.1', 8080)
-try:
-    client_sock.connect(server_addr)
-    print('{},{} 연결 됐습니다.'.format(*server_addr))
+    def connect_to_server(self):
+        try:
+            self.client_sock.connect(self.server_addr)
+            print(f'Connected to {self.server_addr[0]}:{self.server_addr[1]}')
+            self.handle_communication()
+        except Exception as e:
+            print("서버 연결 중 오류 발생:", e)
+        finally:
+            self.client_sock.close()
+            print("클라이언트 종료")
 
-    try:
-        while True:
-            data = input('')
-            if data == 'q':
-                break
-            data = data.encode('cp949')
-            client_sock.send(data)
-            print('메시지를 전송했습니다.')
+    def handle_communication(self):
+        try:
+            while True:
+                data = input('메시지 입력: ')
+                if data == 'q':
+                    break
+                self.send_data(data)
+                self.receive_data()
+        except Exception as e:
+            print("데이터 송수신 중 오류 발생:", e)
 
-            recv_data = client_sock.recv(1024)
+    def send_data(self, data):
+        try:
+            self.client_sock.send(data.encode('cp949'))
+            print('메시지 전송됨.')
+        except Exception as e:
+            print("메시지 전송 중 오류 발생:", e)
+
+    def receive_data(self):
+        try:
+            recv_data = self.client_sock.recv(1024)
             if not recv_data:
                 print("서버로부터 연결이 종료되었습니다.")
-                break
+                return
             print(recv_data.decode('cp949'))
-    except Exception as e:
-        print("데이터 송수신 중 오류 발생:", e)
-finally:
-    client_sock.close()
-    print("클라이언트 종료")
+        except Exception as e:
+            print("메시지 수신 중 오류 발생:", e)
+
+if __name__ == "__main__":
+    chat_client = ChatClient()
+    chat_client.connect_to_server()
--- server_socket.py
+++ server_socket.py
@@ -1,53 +1,62 @@
-from socket import *
+import socket
 import threading
 
-client_count = 0
-lock = threading.Lock()
-is_server_running = True
+class ChatServer:
+    def __init__(self, host='localhost', port=8080):
+        self.server_addr = (host, port)
+        self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_count = 0
+        self.lock = threading.Lock()
+        self.is_server_running = True
+        self.clients = []
 
-def multi_client(client_sock, client_addr, client_count_lock):
-    global client_count, is_server_running
+    def start_server(self):
+        print(f'Starting server on {self.server_addr[0]} port {self.server_addr[1]}')
+        self.server_sock.bind(self.server_addr)
+        self.server_sock.listen()
+        self.accept_clients()
 
-    with client_count_lock:
-        client_count += 1
-
-    print(str(client_addr), '에서 접속이 확인됐습니다.')
-    while True:
-        data = client_sock.recv(1024)
-        if not data:
-            print('클라이언트 연결 종료: ',client_addr)
-            break
-        data = data.decode('cp949')
-        print(f'({client_addr}) 받은 데이터: ',data)
-        client_sock.send(f'{data} has received.'.encode('cp949'))
-    client_sock.close()
-
-    with client_count_lock:
-        client_count -= 1
-        if client_count == 0:
-            print("모든 클라이언트 연결 종료, 서버 종료;")
-            is_server_running = False
-            server_sock.close()
-
-server_sock = socket(AF_INET, SOCK_STREAM)
-server_addr = ('localhost', 8080)
-print('Start up on {} port {}'.format(*server_addr))
-
-server_sock.bind(server_addr)
-server_sock.listen()
-
-try:
-    while is_server_running:
+    def accept_clients(self):
         try:
-            client_sock, client_addr = server_sock.accept()
-            if not is_server_running:
+            while self.is_server_running:
+                try:
+                    client_sock, client_addr = self.server_sock.accept()
+                    if not self.is_server_running:
+                        break
+                    client_thread = threading.Thread(target=self.handle_client, args=(client_sock, client_addr))
+                    client_thread.start()
+                except OSError:
+                    break
+        except Exception as e:
+            print("서버 오류:", e)
+        finally:
+            self.server_sock.close()
+            print("서버 종료")
+
+    def handle_client(self, client_sock, client_addr):
+        with self.lock:
+            self.client_count += 1
+            self.clients.append(client_sock)
+
+        print(f'{client_addr}에서 접속이 확인됐습니다.')
+        while True:
+            data = client_sock.recv(1024)
+            if not data:
+                print('클라이언트 연결 종료:', client_addr)
                 break
-            client_thread = threading.Thread(target=multi_client, args=(client_sock, client_addr, lock))
-            client_thread.start()
-        except OSError:
-            break
-except Exception as e:
-    print("서버 오류:", e)
-finally:
-    server_sock.close()
-    print("서버 종료")
+            data = data.decode('cp949')
+            print(f'({client_addr}) 받은 데이터:', data)
+            client_sock.send(f'{data} has received.'.encode('cp949'))
+
+        client_sock.close()
+        with self.lock:
+            self.client_count -= 1
+            self.clients.remove(client_sock)
+            if self.client_count == 0:
+                print("모든 클라이언트 연결 종료, 서버 종료;")
+                self.is_server_running = False
+                self.server_sock.close()
+
+if __name__ == "__main__":
+    chat_server = ChatServer()
+    chat_server.start_server()
